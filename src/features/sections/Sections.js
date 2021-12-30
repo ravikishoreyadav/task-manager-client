5c2d1897abaf712607fd2aafd9385337044a64d3
sumitmydevit <sumit@mydevitsolutions.com>
2021-12-30 16:59:49 +0530

    working to move task between task

--- src/features/sections/Sections.js
+++ src/features/sections/Sections.js
@@ -1,13 +1,23 @@
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, ButtonGroup, DropdownButton, Dropdown } from 'react-bootstrap';
 import { useSelector, useDispatch} from 'react-redux';
 import {addSection, deleteSection} from './sectionSlice';
-import Task from '../tasks/Tasks';
+//import Task from '../tasks/Tasks';
 import taskSlice from '../tasks/taskSlice';
+import { taskMoved } from '../tasks/taskSlice';
 
 function Sections(){
 
     const sections = useSelector((state) => state.sections)
     const tasks = useSelector((state) => state.tasks)
+
+    const dispatch = useDispatch()
+
+    const onTaskMoved = () => {
+        if (title && content){
+          dispatch(postUpdated({ id: postId, title, content }))
+          //history.push(`/posts/${postId}`)
+        }
+    }
 
     return(
         <>
@@ -25,12 +35,21 @@ function Sections(){
                                         <Card.Title>{filteredTask.title}</Card.Title>
                                         <Card.Text>
                                             {filteredTask.content}
-                                        </Card.Text>                
+                                        </Card.Text>
+                                                        
                                     </Card.Body>
+                                    <Card.Footer>
+                                        <ButtonGroup>
+                                            <DropdownButton as={ButtonGroup} title="Move to" id="bg-nested-dropdown" size="sm">
+                                                <Dropdown.Item eventKey="1" onClick={onTaskMoved}>To Do</Dropdown.Item>
+                                                <Dropdown.Item eventKey="2">In Progress</Dropdown.Item>
+                                            </DropdownButton>
+                                        </ButtonGroup>
+                                    </Card.Footer>                                    
                                 </Card> 
                             ))}
 
-                            </Card.Body>
+                            </Card.Body>                            
                         </Card>
                     </Col>   
                 )) 
--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -4,12 +4,20 @@ export const taskSlice = createSlice({
   name: 'tasks',
   initialState: [
     {id: 1, section_id: 1, title: 'Fix bug', content: 'The bug that goes there should be fixed' },
-    {id: 2, section_id: 3, title: 'Fix bug', content: 'The bug that goes there should be fixed' },
+    {id: 2, section_id: 2, title: 'Fix bug', content: 'The bug that goes there should be fixed' },
   ],
-  reducers: {},
+  reducers: {
+    taskMoved(state, action){
+      const { id, section_id } = action.payload
+      const existingTask = state.find(task => task.id === id)
+      if(existingTask){
+        existingTask.section_id = section_id
+      }
+    }
+  },
 })
 
 // Action creators are generated for each case reducer function
-//export const { addtask, deletetask } = taskSlice.actions;
+export const { taskMoved } = taskSlice.actions;
 
 export default taskSlice.reducer
